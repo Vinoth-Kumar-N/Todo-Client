a6490043aa3425c42ce106e23df3949e265d5c3f
gitvinoth <natarajvinoth7@gmail.com>
2024-11-02 12:49:13 +0530

    Update Login.jsx

--- client/src/pages/Auth/Login.jsx
+++ client/src/pages/Auth/Login.jsx
@@ -30,8 +30,8 @@ const Login = () => {
         }
 
       }).catch((err) =>{
-        console.log(err.response.data.message);
-        toast(err.response.data.message , { type: "error" });
+        console.log(err);
+        toast(err, { type: "error" });
       }) 
   }
 
